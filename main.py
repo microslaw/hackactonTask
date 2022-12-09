fda3f446600902fc35e1bccbf2901ee7e1ac2ac2
michal <mszablewski2004@gmail.com>
2022-12-09 11:49:33 +0100

    added numbersToLetters and deleteDuplicates

--- main.py
+++ main.py
@@ -23,3 +23,27 @@ def areTwoSwapped(toSwap:str, pattern):
     if len(oddLetters) == 2:
         return True
     return False
+
+def numbersToLetters(word: str):
+    numberLetterPairs = {"1":"i",
+    "3": "e",
+    "4": "a",
+    "0": "o",
+    "v": "u",
+    "I": "l",
+    "vv": "w",
+    "ó":"u",
+    "rz":"ż",
+    "ch":"h"
+    }
+    for number in numberLetterPairs:
+        word = word.replace(number, numberLetterPairs[number])
+    return word
+
+def deleteDuplicates(word:str):
+    noDuplicatesWord = ""
+    for i in range(len(word)-1):            
+        if word[i] != word[i+1]:
+            noDuplicatesWord+=word[i]
+    noDuplicatesWord += word[-1]
+    return word
