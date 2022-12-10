543a01ee824ff0fd4e3fd49194d8b05628411614
microslaw <milosz.grunwald@gmail.com>
2022-12-10 16:31:08 +0100

    renamed files and functions, added private functions

--- censoring.py
+++ censoring.py
@@ -1,4 +1,4 @@
-def countDifferences( word1:str, word2:str):
+def __countDifferences( word1:str, word2:str):
     differences = 0
     if(len(word1) == len(word2)):
         for letter1, letter2 in zip(word1,word2):
@@ -9,13 +9,13 @@ def countDifferences( word1:str, word2:str):
             word1, word2 = word2, word1
         j = 0
         for i in range(len(word1)):
-            if word1[i] == word2[j]:
+            if word1[min(i,len(word1)-1)] == word2[min(j,len(word2)-1)]:
                 j+=1
             else:
                 differences+=1
     return differences
     
-def areTwoSwapped(toSwap:str, pattern):
+def __areTwoSwapped(toSwap:str, pattern):
     oddLetters = set()
     for letter1, letter2 in zip(toSwap, pattern):
         if letter1 != letter2:
@@ -24,7 +24,7 @@ def areTwoSwapped(toSwap:str, pattern):
         return True
     return False
 
-def normalizeChars(word: str):
+def __normalizeChars(word: str):
     normalizingChars = {"1":"i",
     "3": "e",
     "4": "a",
@@ -41,7 +41,7 @@ def normalizeChars(word: str):
     return word
 
 
-def deleteDuplicates(word:str):
+def __deleteDuplicates(word:str):
     noDuplicatesWord = ""
     for i in range(len(word)-1):            
         if word[i] != word[i+1]:
@@ -50,13 +50,15 @@ def deleteDuplicates(word:str):
     return word
 
 
-def formatEndings(word:str):
+def __formatEndings(word:str):
     formatList = {"ana":"any",
     "anie": "any",
     "ać": "any",
     "aż": "any",
     "ał": "any",
     "ony": "any",
+    "o":"a",
+    "y":"a"
     }
 
     for ending in formatList:
@@ -65,19 +67,29 @@ def formatEndings(word:str):
     return word
 
 
-def formatWord(word:str):
-    formattedWord = normalizeChars(word)
-    formattedWord = deleteDuplicates(formattedWord)
+def __formatWord(word:str):
+    formattedWord = __normalizeChars(word)
+    formattedWord = __deleteDuplicates(formattedWord)
     formattedWord = formattedWord.lower()
-    formattedWord = formatEndings(formattedWord)
+    formattedWord = __formatEndings(formattedWord)
     return formattedWord
 
 
+badWordsList = []
+with open("badWords.txt", 'r', encoding='utf-8') as rfile:
+    for line in rfile:
+        line = line.strip()                
+        badWordsList.append(line)
+
+    
+
 def isABadWord(word:str):
+    if len(word)<3:
+        return
     for badWord in badWordsList:
-        badWord = formatWord(badWord)
-        word = formatWord(word)
-        differencesCount = countDifferences(word, badWord)
+        badWord = __formatWord(badWord)
+        word = __formatWord(word)
+        differencesCount = __countDifferences(word, badWord)
         if differencesCount == 0:
             return True
         if differencesCount == 1 and (len(word) -1 == len(badWord)):
@@ -86,21 +98,8 @@ def isABadWord(word:str):
             return True
     return False
 
-
-def readFile(filename:str):
-    lines = []
-    with open(filename, 'r', encoding='utf-8') as rfile:
-        for line in rfile:
-            line = line.strip()
-            words = line.split(" ")                
-            lines.append(words)
-    return lines
-
-
 def censorWord(word:str):
     if isABadWord(word):
         word = word[0] + ('*'*(len(word)-2)) + word[-1]
-    return
-
-badWordsList = readFile("badWords.txt")
+    return word
 
